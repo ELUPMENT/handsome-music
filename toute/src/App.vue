929021c382f2a84e619bcec197b06670e879a91e
ELUPMENT <8316942+elupment@user.noreply.gitee.com>
2021-03-02 13:52:31 +0800

    第一次提交

---
+++ toute/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" style="height: 100%">
+    <!--路由占位符-->
+    <router-view ref="hom"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+
+</style>
